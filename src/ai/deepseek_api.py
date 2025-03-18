f87e3670c0e64289179991dd564796b0650e9c6b
Abdul-Rahman-Ammourah <129325480+Abdul-Rahman-Ammourah@users.noreply.github.com>
2025-03-19 00:59:01 +0300

    calling the api to use

--- src/ai/deepseek_api.py
+++ src/ai/deepseek_api.py
@@ -1,4 +1,11 @@
-#import requests 
+import requests 
+from dotenv import load_dotenv # type: ignore
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+deep_seek_api_key = os.getenv('DEEP-SEEK-API')
 
 def deepseek_api():
     # Connect to DeepSeek API
